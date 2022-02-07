e4d3c1ed3bbe18186f46d80a662a48fdfed8baaa
ChristelleM28 <christellemonvoisin@orange.fr>
2022-02-07 14:09:32 +0100

    ajustement carousel version mobile

--- src/components/Footer/FooterMenuContent.jsx
+++ src/components/Footer/FooterMenuContent.jsx
@@ -6,6 +6,9 @@ function FooterMenuContent() {
   return (
     <div className="footerMenuContent">
       <nav>
+        <Link to="/" className="linkHome">
+          Home
+        </Link>
         <Link to="/about" className="linkAbout">
           About
         </Link>
