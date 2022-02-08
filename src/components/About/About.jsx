60c8df19e2e084d95fc7fdfa6fa30d8b8fdd1671
ChristelleM28 <christellemonvoisin@orange.fr>
2022-02-08 17:10:53 +0100

    affichage pdf

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
 import Footer from "../Footer/Footer";
 import cv from "../../assets/cv.pdf";
 import "./About.css";
 
 function About() {
-const handleClick = (e) => { <img src={cv} alt="CV" ></img>};
+  //j'initialise le state de mes évènements
+  const [displayPdf, setDisplayPdf] = useState(false);
+
+// je définie l'évènement sur lequel fonctionne le clic
+  const handleClick = (e) => {
+    setDisplayPdf(true);
+  };
 
   return (
     <div className="container">
-      <div className="socialMedia">
-        media sociaux ici
-      </div>
+      <div className="socialMedia">media sociaux ici</div>
       <div className="rectangleAbout">
         <div className="aboutMe">
           <h2>ABOUT ME</h2>
@@ -19,39 +23,36 @@ const handleClick = (e) => { <img src={cv} alt="CV" ></img>};
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
             blanditiis voluptatibus impedit dignissimos accusantium illo fuga
             perspiciatis, voluptas vel unde? Quae soluta eaque totam neque
-            doloribus sit nam voluptas.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
+            doloribus sit nam voluptas. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Quo facilis blanditiis voluptatibus impedit
+            dignissimos accusantium illo fuga perspiciatis, voluptas vel unde?
+            Quae soluta eaque totam neque doloribus sit nam voluptas. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
+            blanditiis voluptatibus impedit dignissimos accusantium illo fuga
+            perspiciatis, voluptas vel unde? Quae soluta eaque totam neque
+            doloribus sit nam voluptas. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Quo facilis blanditiis voluptatibus impedit
+            dignissimos accusantium illo fuga perspiciatis, voluptas vel unde?
+            Quae soluta eaque totam neque doloribus sit nam voluptas. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
+            blanditiis voluptatibus impedit dignissimos accusantium illo fuga
+            perspiciatis, voluptas vel unde? Quae soluta eaque totam neque
+            doloribus sit nam voluptas. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Quo facilis blanditiis voluptatibus impedit
+            dignissimos accusantium illo fuga perspiciatis, voluptas vel unde?
+            Quae soluta eaque totam neque doloribus sit nam voluptas. Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
             blanditiis voluptatibus impedit dignissimos accusantium illo fuga
             perspiciatis, voluptas vel unde? Quae soluta eaque totam neque
             doloribus sit nam voluptas.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
-            blanditiis voluptatibus impedit dignissimos accusantium illo fuga
-            perspiciatis, voluptas vel unde? Quae soluta eaque totam neque
-            doloribus sit nam voluptas.
-                      
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
-            blanditiis voluptatibus impedit dignissimos accusantium illo fuga
-            perspiciatis, voluptas vel unde? Quae soluta eaque totam neque
-            doloribus sit nam voluptas.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
-            blanditiis voluptatibus impedit dignissimos accusantium illo fuga
-            perspiciatis, voluptas vel unde? Quae soluta eaque totam neque
-            doloribus sit nam voluptas.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
-            blanditiis voluptatibus impedit dignissimos accusantium illo fuga
-            perspiciatis, voluptas vel unde? Quae soluta eaque totam neque
-            doloribus sit nam voluptas.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo facilis
-            blanditiis voluptatibus impedit dignissimos accusantium illo fuga
-            perspiciatis, voluptas vel unde? Quae soluta eaque totam neque
-            doloribus sit nam voluptas.
-</p>
+          </p>
         </div>
-        <Button title="My CV" more="Let's go!" onClick={handleClick}/>
-        </div>
-      <Footer/>
+        <Button title="My CV" more="Let's go!" onClick={handleClick} />
+      </div>
+      {/* je définie la condition d'affichage de mon pdf */}
+      {displayPdf ? <iframe src={cv} title="cv"></iframe> : ""}
+      <Footer />
     </div>
-    
   );
 }
 
