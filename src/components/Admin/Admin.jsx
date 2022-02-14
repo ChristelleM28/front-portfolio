3990f318e0f13fa370d9b6c08c167154b16f7857
ChristelleM28 <christellemonvoisin@orange.fr>
2022-02-14 09:12:38 +0100

    MAJ

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -3,12 +3,31 @@ import { Navigate } from "react-router-dom";
 import "./Admin.css";
 import Button from "../Button/Button";
 
+const API_URL = process.env.REACT_APP_API_PORTFOLIO_URL;
 
 function Admin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const [followLink, setFollowLink] = useState(false);
   const handleClick = () => {
     setFollowLink(!followLink);
   };
+
+  const login = async () => {
+    if (email && password) {
+      try{
+        const response = await axios.post (`${process.env.API_URL}/api/auth/admin`, { email, password });
+  };
+  return response.data;
+  console.log(response.data);
+} catch (err) {
+console.log(err.response.data);
+}
+  } else {
+    alert{"Renseigner une valeur"};
+  }
+};
 
   return (
       <div>
