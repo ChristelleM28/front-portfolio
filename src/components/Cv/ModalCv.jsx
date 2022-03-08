2b3a42e161ab60a121835b58c18ee37c26342ed7
ChristelleM28 <christellemonvoisin@orange.fr>
2022-03-08 17:46:43 +0100

    modale mobile

--- src/components/Cv/ModalCv.jsx
+++ src/components/Cv/ModalCv.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ModalCv.css";
 import cv from "../../assets/cv.pdf";
+import myCV from "../../assets/myCV.jpg";
 
 function ModalCv({ onClick }) {
+  const [displayIframe, setDisplayIframe] = useState(null);
+  const [displayImg, setDisplayImg] = useState(true);
+  
   return (
     <div className="modalCvContainer">
-      <button onClick={onClick} >Close</button>
-      <div classeName="iframeCv">
-        <iframe src={cv} title="cv" type="application/pdf" frameborder="0" height="100%" width="100%"></iframe>
+      <button onClick={onClick}>Close</button>
+      <div className="modal">
+        {displayIframe ? setDisplayIframe : setDisplayImg}
+        <iframe classeName="iframeCv"
+          src={cv}
+          title="cv"
+          type="application/pdf"
+          frameborder="0"
+          height="100%"
+          width="100%"
+        ></iframe>
+        <img className="CVImg" src={myCV} alt="CV"></img>
       </div>
     </div>
   );
